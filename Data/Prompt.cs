e1654350759a6f8ef98b57cb65c7adf620fceb06
KyleDickersonComposer <44070073+KyleDickersonComposer@users.noreply.github.com>
2024-01-14 08:39:24 -0500

    first push

---
+++ Data/Prompt.cs
@@ -0,0 +1,26 @@
+public class Prompt
+{
+    List<string> genres = new List<string>
+{
+     "orchestral", "dark sci-fi", "retro sci-fi", "fairymusic", "arabesque", "americana", "trash beats","fantasy", "sci-fi", "sports", "popsy", "modern fantasy", "postmodern", "dark fantasy", "adventure", "lament", "comedy", "romance", "crime", "horror", "thriller", "family friendly", "western", "stealth", "mystery"
+};
+    List<string> prompt = new List<string>
+{
+     "dance", "scherzo", "battle", "explore", "action", "ambient", "ballad", "chase", "love", "uplifting", "inspirational", "quirky", "underwater", "fire", "space", "rain", "volcano", "death", "fear", "legere", "filigre"
+};
+    public async Task<string> PromptData()
+    {
+
+        var rand = new Random();
+        string data = $"Write a short piece with the prompt {prompt[rand.Next(1, prompt.Count)]} in the genre {genres[rand.Next(1, genres.Count)]}.";
+        Console.WriteLine(data);
+
+        return data;
+    }
+}
+
+
+
+
+
+
