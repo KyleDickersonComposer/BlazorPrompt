66d3a50725b6f7c931b33ec8f991ac4f0cb43937
KyleDickersonComposer <44070073+KyleDickersonComposer@users.noreply.github.com>
2024-01-30 10:11:05 -0500

    fixed some warnings

--- Data/Prompt.cs
+++ Data/Prompt.cs
@@ -2,20 +2,20 @@ public class Prompt
 {
     List<string> genres = new List<string>
 {
-     "orchestral", "dark sci-fi", "retro sci-fi", "fairymusic", "arabesque", "americana", "trash beats","fantasy", "sci-fi", "sports", "popsy", "modern fantasy", "postmodern", "dark fantasy", "adventure", "lament", "comedy", "romance", "crime", "horror", "thriller", "family friendly", "western", "stealth", "mystery"
+     "orchestral", "dark sci-fi", "retro sci-fi", "fairy music", "arabesque", "americana", "trash beats","fantasy", "sci-fi", "sports", "popsy", "modern fantasy", "postmodern", "dark fantasy", "adventure", "lament", "comedy", "romance", "crime", "horror", "thriller", "family friendly", "western", "stealth", "mystery"
 };
     List<string> prompt = new List<string>
 {
      "dance", "scherzo", "battle", "explore", "action", "ambient", "ballad", "chase", "love", "uplifting", "inspirational", "quirky", "underwater", "fire", "space", "rain", "volcano", "death", "fear", "legere", "filigre"
 };
-    public async Task<string> PromptData()
+    public Task<string> PromptData()
     {
 
         var rand = new Random();
         string data = $"Write a short piece with the prompt {prompt[rand.Next(0, prompt.Count)]} in the genre {genres[rand.Next(0, genres.Count)]}.";
         Console.WriteLine(data);
 
-        return data;
+        return Task.FromResult(data);
     }
 }
 
